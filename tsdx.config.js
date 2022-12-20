dc1c3f777c1c7b7f9b2a8a5185f56581104db3fb
william Cabrera <williamjnr8708@gmail.com>
2022-12-20 13:24:59 -0500

    update

--- tsdx.config.js
+++ tsdx.config.js
@@ -24,7 +24,7 @@ module.exports = {
     );
 
     config.plugins = [
-      images({ incude: ['**/*.png', '**/*.jpg'] }),
+      //images({ incude: ['**/*.png', '**/*.jpg'] }),
       image(),
       ...config.plugins,
     ];
